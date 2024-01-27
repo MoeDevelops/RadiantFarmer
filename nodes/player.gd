48be343cb873fcc9d404c721f561d15f04a9ea62
MoeDev <niesenmoritz@gmail.com>
2024-01-27 13:19:48 +0100

    Initial commit

---
+++ nodes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var max_speed: float = 120
+@export var acceleration: float = 10
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(_delta):
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	
+	velocity.x = move_toward(velocity.x, max_speed * direction.x, acceleration)
+	velocity.y = move_toward(velocity.y, max_speed * direction.y, acceleration)
+	
+	if velocity.x == 0 and velocity.y == 0:
+		sprite.play("Idle")
+	else:
+		sprite.play("Walk")	
+		sprite.flip_h = velocity.x < 0
+			
+
+	move_and_slide()
